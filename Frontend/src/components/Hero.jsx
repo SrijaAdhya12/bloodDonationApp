1e4fe2a2984d8c4a9e56e29f3dd8fa2ac31504bd
Srija Adhya <113666002+SrijaAdhya12@users.noreply.github.com>
2024-10-02 13:22:24 +0530

    feat: added hero section

---
+++ Frontend/src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import illustration from '../assets/illustration.png'
+const Hero = () => {
+  return (
+		<div className="flex justify-between items-center  mx-auto container">
+			<div className=" text-white w-1/2">
+				<span className="text-2xl mt-3 text-red-500 font-bold">
+					Donate blood, Save life!
+				</span>
+				<h1 className="text-6xl leading-normal mt-3 text-black">
+					Your <span className='text-red-500 font-bold'>Blood 🩸</span> Can Bring Smile In Other Person Life.
+				</h1>
+
+				<div className="flex items-center gap-10 my-10">
+					<button className="bg-red-500 p-4 w-[200px] rounded-lg text-white cursor-pointer">
+						Donate Now
+					</button>
+					<button className="bg-gray-500 p-4 w-[200px] rounded-lg text-white cursor-pointer">
+						CALL : (+125) 262 728
+					</button>
+				</div>
+			</div>
+			<img className="w-[500px]" src={illustration} alt="" />
+		</div>
+	);
+}
+
+export default Hero
---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-scroll";
+
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center gap-10 h-[100px] px-[200px] mb-16">
+      <img
+        src="/logo1.png"
+        alt=""
+        height={200}
+        width={200}
+        className="cursor-pointer"
+      />
+
+      <div className="flex items-center gap-10 cursor-pointer">
+        <Link to="hero" smooth={true} duration={1000} className="mr-3 text-[18px] font-medium">Home</Link>
+        <Link to="featured" smooth={true} duration={1000} className="mr-3 text-[18px] font-medium">About Us</Link>
+        <Link to="contact" smooth={true} duration={1000} className="mr-3 text-[18px] font-medium">Contact Us</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
